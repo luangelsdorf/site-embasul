060c6f0ecbe00d6419db32b1fc2982bf39b0fedb
luangelsdorf <luanferreira2136@gmail.com>
2023-12-06 10:12:08 -0300

    Slick setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,7 @@
 import '@/styles/globals.css';
+import '@/styles/slick.css';
+import '@/styles/slick-theme.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- src/pages/empresa.js
+++ src/pages/empresa.js
@@ -1,7 +1,59 @@
+import Slider from "react-slick";
+
 export default function Company() {
+
+  let settings = {
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    slide: 'article',
+    centerMode: true,
+
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          centerMode: false,
+        },
+      },
+    ],
+  };
+
   return (
-    <div>
-      <h1>Empresa</h1>
+    <div className="container">
+      <Slider {...settings}>
+        <article>
+          <div style={{ margin: '0 12px' }}>
+            <h3 style={{ height: '500px', backgroundColor: 'grey' }}>1</h3>
+          </div>
+        </article>
+        <article>
+          <div style={{ margin: '0 12px' }}>
+            <h3 style={{ height: '500px', backgroundColor: 'red' }}>2</h3>
+          </div>
+        </article>
+        <article>
+          <div style={{ margin: '0 12px' }}>
+            <h3 style={{ height: '500px', backgroundColor: 'blue' }}>3</h3>
+          </div>
+        </article>
+        <article>
+          <div style={{ margin: '0 12px' }}>
+            <h3 style={{ height: '500px', backgroundColor: 'green' }}>4</h3>
+          </div>
+        </article>
+        <article>
+          <div style={{ margin: '0 12px' }}>
+            <h3 style={{ height: '500px', backgroundColor: 'tomato' }}>5</h3>
+          </div>
+        </article>
+        <article>
+          <div style={{ margin: '0 12px' }}>
+            <h3 style={{ height: '500px', backgroundColor: 'purple' }}>6</h3>
+          </div>
+        </article>
+      </Slider>
     </div>
-  )
+  );
 }

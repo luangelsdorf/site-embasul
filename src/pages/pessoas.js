27569df5807f4692fb97425fdbf14e08ed0f352d
luangelsdorf <luanferreira2136@gmail.com>
2023-12-08 18:42:37 -0300

    People page

--- src/pages/pessoas.js
+++ src/pages/pessoas.js
@@ -1,10 +1,9 @@
-import banner from 'public/images/gambi/sustain/Banner.png';
-import fsc from 'public/images/gambi/sustain/fsc.png';
-import origem from 'public/images/gambi/sustain/origem-sustentavel.png';
-import pegada from 'public/images/gambi/sustain/pegada-neutra.png';
-import cts from 'public/images/gambi/sustain/cts.png';
-import politica from 'public/images/gambi/sustain/politica.png';
+import trabalhe from 'public/images/gambi/people/trabalhe-conosco.png';
+import pessoas from 'public/images/gambi/people/pessoas.png';
+import canal from 'public/images/gambi/people/canal-etica.png';
+import codigo from 'public/images/gambi/people/codigo-etica.png';
 import Head from 'next/head';
+import Image from 'next/image';
 
 export default function People() {
   return (
@@ -13,12 +12,10 @@ export default function People() {
         <title>Pessoas | Embasul</title>
       </Head>
 
-      <Image style={{ width: '100%', height: 'auto', objectPosition: 'top', objectFit: 'cover' }} src={banner} />
-      <Image id="fsc" style={{ width: '100%', height: 'auto', objectPosition: 'top', objectFit: 'cover' }} src={fsc} />
-      <Image id="origem-sustentavel" style={{ width: '100%', height: 'auto', objectPosition: 'top', objectFit: 'cover' }} src={origem} />
-      <Image id="pegada-neutra.png" style={{ width: '100%', height: 'auto', objectPosition: 'top', objectFit: 'cover' }} src={pegada} />
-      <Image id="cts" style={{ width: '100%', height: 'auto', objectPosition: 'top', objectFit: 'cover' }} src={cts} />
-      <Image id="politica" style={{ width: '100%', height: 'auto', objectPosition: 'top', objectFit: 'cover' }} src={politica} />
+      <Image id="trabalhe-conosco" style={{ width: '100%', height: 'auto', objectPosition: 'top', objectFit: 'cover' }} src={trabalhe} />
+      <Image id="pessoas" style={{ width: '100%', height: 'auto', objectPosition: 'top', objectFit: 'cover' }} src={pessoas} />
+      <Image id="canal-etica" style={{ width: '100%', height: 'auto', objectPosition: 'top', objectFit: 'cover' }} src={canal} />
+      <Image id="codigo-etica" style={{ width: '100%', height: 'auto', objectPosition: 'top', objectFit: 'cover' }} src={codigo} />
     </div>
   )
 }

90203a2e537466cad4be616f37b153fff370dd31
luangelsdorf <luanferreira2136@gmail.com>
2023-12-08 19:16:01 -0300

    Company page

--- src/pages/empresa.js
+++ src/pages/empresa.js
@@ -1,7 +1,7 @@
-import Slider from "react-slick";
-import Structure from 'public/images/svg/Estrutura.svg';
 import Head from "next/head";
-import banner from 'public/images/svg/empresa/page.png'
+import estrutura from 'public/images/gambi/company/estrutura.png';
+import historia from 'public/images/gambi/company/historia.png';
+import norteadores from 'public/images/gambi/company/norteadores.png';
 import Image from "next/image";
 
 export default function Company() {
@@ -15,7 +15,9 @@ export default function Company() {
 
       {/* <Structure style={{ width: '100%', height: 'auto', }} /> */}
       {/* <div style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '80px', background: '#006CCC'}} /> */}
-      <Image quality={100} style={{ width: '100%', height: 'auto', objectPosition: 'top', objectFit: 'cover' }} src={banner} />
+      <Image id="estrutura" quality={100} style={{ width: '100%', height: 'auto', objectPosition: 'top', objectFit: 'cover' }} src={estrutura} />
+      <Image id="historia" quality={100} style={{ width: '100%', height: 'auto', objectPosition: 'top', objectFit: 'cover' }} src={historia} />
+      <Image id="norteadores" quality={100} style={{ width: '100%', height: 'auto', objectPosition: 'top', objectFit: 'cover' }} src={norteadores} />
     </>
   );
 }

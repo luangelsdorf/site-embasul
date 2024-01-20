a62fd03ede070a78cf2642909b47333af618b3d4
luangelsdorf <luanferreira2136@gmail.com>
2024-01-20 17:32:17 -0300

    DropdownMenu component

---
+++ src/components/layout/Header/DropdownMenu/index.js
@@ -0,0 +1,26 @@
+import styles from './DropdownMenu.module.scss';
+
+export default function DropdownMenu({ children, extraLinks }) {
+  return (
+    <div className={styles.dropDownMenu}>
+      <ul data-dd-menu>
+        {
+          children.map ? (
+            children.map((child, i) => <li key={i}>{child}</li>)
+          ) : (
+            children
+          )
+        }
+      </ul>
+      <div className={styles.extraLinks}>
+        {
+          extraLinks && extraLinks.map((link, i) => (
+            <div key={i}>
+              {link}
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  )
+}

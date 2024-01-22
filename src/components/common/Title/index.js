c1760016dcc46d27a2bbde3c9922eff51200ff8a
luangelsdorf <luanferreira2136@gmail.com>
2024-01-22 08:55:05 -0300

    Title component

---
+++ src/components/common/Title/index.js
@@ -0,0 +1,20 @@
+import styles from './Title.module.scss';
+
+export default function Title({ content, align = 'start', spacing = 24, variant, children }) {
+  const { title, overline } = content;
+
+  return (
+    <header className={`${styles.title} ${styles[variant] ?? ''} ${styles[align] ?? ''}`} style={{ gap: `${spacing}px` }}>
+      {
+        children ? (
+          children
+        ) : (
+          <>
+            {overline && <p>{overline}</p>}
+            <h2>{title}</h2>
+          </>
+        )
+      }
+    </header>
+  )
+}

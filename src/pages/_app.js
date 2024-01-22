f100dc2e91a75b051a8e2a7602975174e389e1c9
luangelsdorf <luanferreira2136@gmail.com>
2024-01-22 07:36:05 -0300

    Config changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,16 @@
 import '@/styles/cargotemplate.css';
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import '@/styles/style.scss';
 import { LayoutContext } from '@/utils/contexts';
+import Header from '@/components/layout/Header';
 
 export default function App({ Component, pageProps }) {
 
   if ((!pageProps.layout) && (Object.keys(pageProps).length > 0)) console.warn('[Warning]: Layout info not passed into `_app.js`');
-  
+
   return (
     <LayoutContext.Provider value={pageProps.layout}>
+      <Header />
       <Component {...pageProps} />
     </LayoutContext.Provider>
   )

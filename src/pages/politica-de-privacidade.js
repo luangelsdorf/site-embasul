d687c6c3ad6fc4ce72f295970b1f2650cc454179
luangelsdorf <luanferreira2136@gmail.com>
2024-01-30 10:25:31 -0300

    Blank policy page

---
+++ src/pages/politica-de-privacidade.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import React from 'react'
+
+export default function PrivacyPolicy() {
+  return (
+    <div>
+      <Head>
+        <title>Política de Privacidade - Embasul</title>
+      </Head>
+      <div style={{ height: '100vh' }} />
+    </div>
+  )
+}

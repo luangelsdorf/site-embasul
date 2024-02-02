689febe858591f54b51c5eb1d2b600f4537e3226
luangelsdorf <luanferreira2136@gmail.com>
2024-02-02 10:40:40 -0300

    Added text highlighting feature

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -13,3 +13,23 @@ export function getExcerpt(string, wordLimit = 24) {
 }
 
 export const hasCategory = (post, category = '') => !!post.attributes.categories.data.filter(cat => cat.attributes.slug === category).length;
+
+export function toFormatted(text = '') {
+  const phrases = text
+    .replaceAll('/*', '$')
+    .split('*');
+
+  const markedPhrases = phrases.map((phrase, i) => {
+    if (i % 2 === 1) {
+      return <b>{phrase.replaceAll('$', '*')}</b>;
+    } else {
+      return <>{phrase.replaceAll('$', '*')}</>;
+    }
+  })
+
+  return (
+    <>
+      {markedPhrases.map((p, i) => <span key={i}>{p}</span>)}
+    </>
+  );
+}

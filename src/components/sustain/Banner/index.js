f4e11ee8850f4385c94bd439961c4eb4080593cb
luangelsdorf <luanferreira2136@gmail.com>
2024-02-14 08:03:09 -0300

    Sustainability page animations

--- src/components/sustain/Banner/index.js
+++ src/components/sustain/Banner/index.js
@@ -2,14 +2,18 @@ import Img from '@/components/common/Img';
 import styles from './Banner.module.scss';
 import { getSizesString } from '@/utils/images';
 import { toFormatted } from '@/utils/helpers';
+import { animateBanner, slideUp } from '@/utils/animation';
+import Reveal from 'react-awesome-reveal';
 
 export default function Banner({ content }) {
-  
+
   return (
     <div className={styles.section}>
-      <Img {...content.icon} fill sizes={getSizesString('col-12 col-lg-10')} alt="" />
+      <Img style={{ opacity: 0 }} onLoad={animateBanner} {...content.icon} fill sizes={getSizesString('col-12 col-lg-10')} alt="" />
       <div className={styles.textContent}>
-        <h1 className="display-1">{toFormatted(content.text)}</h1>
+        <Reveal triggerOnce keyframes={slideUp} duration={500} fraction={0.5}>
+          <h1 className="display-1">{toFormatted(content.text)}</h1>
+        </Reveal>
       </div>
     </div>
   )
--- src/components/sustain/Policy/index.js
+++ src/components/sustain/Policy/index.js
@@ -4,6 +4,8 @@ import Button from '@/components/common/Button';
 import DownloadIcon from 'public/images/icons/download.svg';
 import Img from '@/components/common/Img';
 import { toFormatted } from '@/utils/helpers';
+import Reveal from 'react-awesome-reveal';
+import { slideUp } from '@/utils/animation';
 
 export default function Policy({ content }) {
   const colors = [
@@ -21,31 +23,35 @@ export default function Policy({ content }) {
         <div className="row justify-content-center">
           <div className="col-12">
             <header>
-              <p className="overline">{content.headline.overline}</p>
-              <h2 className="display-1">{content.headline.title}</h2>
+              <Reveal triggerOnce keyframes={slideUp} duration={500} fraction={0.5} cascade damping={0.4}>
+                <p className="overline">{content.headline.overline}</p>
+                <h2 className="display-1">{content.headline.title}</h2>
+              </Reveal>
             </header>
           </div>
-          <div className="col-12 col-lg-6">
+          <Reveal triggerOnce keyframes={slideUp} duration={500} fraction={0.5} delay={200} className="col-12 col-lg-6">
             <div className={styles.text}>
               <p>{toFormatted(content.text)}</p>
             </div>
-          </div>
-          <div className="col-12 col-lg-4">
+          </Reveal>
+          <Reveal triggerOnce keyframes={slideUp} duration={500} fraction={0.5} delay={400} className="col-12 col-lg-4">
             <div className={styles.download}>
               <p className="heading-h3-size">{content.cta}</p>
               <Button className="btn-secondary" LeftIcon={DownloadIcon} href={content.downloadLink.url}>{content.downloadLink.text}</Button>
             </div>
-          </div>
+          </Reveal>
           <div className="col-12 col-lg-10">
             <ul className={styles.items}>
-              {
-                content.items.map((item, i) => (
-                  <li key={item.id} style={{backgroundColor: colors[i]}}>
-                    <div className="heading-h3-size">{item.text}</div>
-                    <Img {...item.icon} />
-                  </li>
-                ))
-              }
+              <Reveal triggerOnce keyframes={slideUp} duration={500} fraction={0.5} cascade damping={0.15}>
+                {
+                  content.items.map((item, i) => (
+                    <li key={item.id} style={{backgroundColor: colors[i]}}>
+                      <div className="heading-h3-size">{item.text}</div>
+                      <Img {...item.icon} />
+                    </li>
+                  ))
+                }
+              </Reveal>
             </ul>
           </div>
         </div>
